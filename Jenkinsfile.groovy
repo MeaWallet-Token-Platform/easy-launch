5ed63810c416b4ab60e2751be374dbd2654721e4
Yevhen Duma <yevhen.duma@gmail.com>
2021-07-13 10:38:39 +0300

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+@Library("standardPipelineFunctions@release-1.4.23") _
+properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '60', numToKeepStr: '20']]]);
+
+standardFrontendBuild {
+    nexusArtifactName = 'easy-launch'
+    sourceDirectory = 'dist'
+    buildNode = 'int-linux-t3medium'
+    publish = true
+    deploy = false
+    build = true
+    confluenceParent = '2558656513'
+    confluencePrefix = 'easy-launch'
+}
